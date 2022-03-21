d94172c1f5e489a72062fdb49c4a3f5cd2ce5b7f
Anish-guragain <anishguragain8@gmail.com>
2022-03-21 20:37:55 +0545

    border change

--- script.js
+++ script.js
@@ -19,13 +19,62 @@ anime({
 //Today Top gifter
 
 const account1 = {
-  movements: [0, '@joeyisno2yt:  1050', '@kristee_anna22:  1000'],
+  movements: [
+    0,
+    'divineemperas:2300',
+    'kingbigcountry:1700',
+    'unique:1300',
+    'anisha:100',
+  ],
 };
 
 // All time top gifter
 
 const account2 = {
-  movements: [0, '@joeyisno2yt:  1600'],
+  movements: [
+    0,
+    // 'dania_hassam: unlimited',
+    // 'editz_forme23: 35000',
+    'mickaylagraf5:59300',
+    'matthewb158:40500',
+    'tiere_von_uns: 28000',
+    't_roxy45:18500',
+    'muliganbeauty:  25000',
+    '@italiasian: 17800',
+    '@kristee_anna22:  15000',
+
+    'twinmiracles:12000',
+    'benyoub: 10000',
+
+    'sue652002:8000',
+    'jasminearely:7600',
+    'starbrust_107: 7200',
+    'brandonlol28: 6600',
+    'missnala31:6100',
+    't_roxy:6000',
+    'terrie.rains:5000',
+    'fnfmusic345:5000',
+    'mikki_b72:5000',
+    'jmrsp:4500',
+    'mr._bling:4100',
+    'jagga2244:4003',
+    'kimberlygrace777:4000',
+    'goldstartfamily:4000',
+
+    'loveleen_army:3600',
+    'gon___1:3500',
+    'batg4rl: 3400',
+    'miamia_jelouspotato:3000',
+
+    'anika_grey:3600',
+    'maddyjermy:2500',
+    '@hillarybenton:2100',
+    'aboveall313:1300',
+    '@joeyisno2yt:  1050',
+    '@marshallmoore:  1000',
+    '@kristee_anna22:  1000',
+    '@joeyisno2yt:  1600',
+  ],
 };
 
 var x = 0;
